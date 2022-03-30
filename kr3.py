32233fdbafc0f624c98aeb53fa6dbc3ef852bacf
MeeILove <itagibov@bk.ru>
2022-03-30 13:38:15 +0300

    added some changes

--- kr3.py
+++ kr3.py
@@ -2,8 +2,8 @@
 
 import random
 
-hunger_list = ['Сытый', 'Проголодался', 'Голодный']
-boredom_list = ['Счастлив', 'Так себе', 'Уныние']
+hunger_list = [1, 2, 3] #1 - хорошо, 2 - средне, 3 - плохо 
+boredom_list = [1, 2, 3] #1 - хорошо, 2 - средне, 3 - плохо 
 names_list = ['Барсик', 'Шарик', 'Мурзик', 'Бобик', 'Тузик', 'Бим', 'Тим']
 
 class Critter():
@@ -12,28 +12,95 @@ class Critter():
         self.name = random.choice(names_list)
         self.hunger = random.choice(hunger_list)
         self.boredom = random.choice(boredom_list)
+    def __pass_time(self):
+        self.hunger += 1
+        self.boredom += 1
+    def eat(self, food):
+        food = int(input('Какое кол-во еды хотите дать (от 1 до 3)?'))
+        print('Мммм, вкусно, Спасибо!')
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+    def play(self, game):
+        game = int(input('Какое кол-во времени вы хотите поиграть(от 1 до 3)?'))
+        print('Вау, круто, Спасибо!')
+        self.boredom -= game
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
     def mood(self, mood):
-        mood = ""
-        if self.hunger == 'Сытый' and self.boredom == 'Счастлив':
+        moody = self.hunger + self.boredom
+        if moody < 2:
             mood = "Отлично"
-        elif self.hunger == 'Проголодался' and self.boredom == 'Так себе':
+        elif  2<moody<=4:
             mood = "Нормально"
-        elif self.hunger == 'Голодный' and self.boredom == 'Уныние':
-            mood = "Грустно"
+        elif 4<moody<=9:
+            mood = "Грустно, мне нужны внимание и забота"
         else:
             mood = 'Мне нужны внимание и забота'
         return mood
     def talk(self):
         print('Меня зовут '+ self.name)
-        print('Сейчас я ' + self.hunger)
-        print(self.boredom + ' - моё состояние')
+        print('Уровень голода - ' , self.hunger)
+        print('Уровень уныния - ' , self.boredom)
         print("Настроение - ", end = '')
+        self.__pass_time()
+
+    # choice = None
+    # while choice != 0:
+    #     print \
+    #         ('''
+    #         Моё животное 
+
+    #         0 - выйти
+    #         1 - узнать о самочувствии животного
+    #         2 - покормить животное
+    #         3 - поиграть с животным 
+    #         ''')
+    #     choice = input('Ваш выбор: ')
+    #     print()
+    #     if choice == '0':
+    #         print('До свидания!')
+    #     elif choice == '1':
+    #         crit.talk()
+    #     elif choice == '2':
+    #         crit.eat()
+    #     elif choice == '3':
+    #         crit.play()
+    #     else:
+    #         print('\nИзвините, неверная команда', choice)
 
 
 
 
 
-crit1 = Critter()
 
-crit1.talk()
-print(crit1.mood(mood= ""))
+
+crit = Critter()
+
+crit.talk()
+print(crit.mood(mood= ""))
+
+choice = None
+while choice != 0:
+    print \
+        ('''
+        Моё животное 
+        0 - выйти
+        1 - узнать о самочувствии животного
+        2 - покормить животное
+        3 - поиграть с животным 
+        ''')
+choice = input('Ваш выбор: ')
+print()
+if choice == '0':
+    print('До свидания!')
+elif choice == '1':
+    crit.talk()
+elif choice == '2':
+    crit.eat()
+elif choice == '3':
+    crit.play()
+else:
+    print('\nИзвините, неверная команда', choice)
